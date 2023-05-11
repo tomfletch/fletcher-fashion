b1d4730c40a79108bec2bb0335e1b50431656d0d
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2023-05-12 00:01:10 +0100

    Configure for Google Cloud Run

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const path = require('path');
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.get("/api", (req, res) => {
+  res.json({ message: "Fletcher Fashion API" });
 });
 
-app.get("/", (req, res) => {
-  res.json({ message: "Fletcher Fashion API" });
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+app.get('**', function (req, res) {
+  console.log('here');
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
